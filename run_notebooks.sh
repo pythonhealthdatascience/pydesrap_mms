23873eecf2e9dbe936d28a2db4e57f45fa2ef3d2
amyheather <a.heather2@exeter.ac.uk>
2025-03-20 14:12:12 +0000

    feat(notebooks): re-run notebooks following fix to nurse time (which does mean some minor results changes to utilisation) (+ change to bash script metadata settings)

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -7,8 +7,10 @@ for nb in notebooks/*.ipynb; do
     # Execute and update the notebook in-place
     # With some processing to remove metadata created by nbconvert
     if python -m jupyter nbconvert --to notebook --inplace --execute \
-        -TagRemovePreprocessor.remove_input_tags="execution" \
-        --ClearMetadataPreprocessor.enabled=True "$nb"; then
+        --ClearMetadataPreprocessor.enabled=True \
+        --ClearMetadataPreprocessor.preserve_nb_metadata_mask="language_info" \
+        --ClearMetadataPreprocessor.preserve_nb_metadata_mask="kernelspec" \
+        "$nb"; then
         echo "✅ Successfully processed: $nb"
     else
         echo "❌ Error processing: $nb"
