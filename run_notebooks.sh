7a369def5950879c18af32cd954e17ea96e0d6dd
amyheather <a.heather2@exeter.ac.uk>
2025-06-16 14:35:50 +0100

    fix(bash/env): when rebuilt env, had issue with not using the environment's jupyter - add line to get jupyter path, and add jupyter to environment (plus renamed environment)

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Get the conda environment's jupyter path
+CONDA_JUPYTER=$(dirname "$(which python)")/jupyter
+
 # Loop through all notebooks in the specified directory
 for nb in notebooks/*.ipynb; do
     echo "🏃 Running notebook: $nb"
     
     # Execute and update the notebook in-place
     # With some processing to remove metadata created by nbconvert
-    if python -m jupyter nbconvert --to notebook --inplace --execute \
+    if "${CONDA_JUPYTER}" nbconvert --to notebook --inplace --execute \
         --ClearMetadataPreprocessor.enabled=True \
         --ClearMetadataPreprocessor.clear_notebook_metadata=False \
         --ClearMetadataPreprocessor.preserve_cell_metadata_mask="kernelspec" \
